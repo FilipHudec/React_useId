cf33334d94fefb78ba6019e39b17ff076e92925b
Filip Hudec <filip.hudec.022@pslib.cz>
2024-09-27 09:31:04 +0200

    funny

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import InputNumber from './components/InputNumber'
@@ -7,9 +7,11 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
- if(count % 2 === 0) {
-  return <p>Odd number</p>
+  const display: Array<ReactNode> = []
+ if(count > 200) {
+  return <p>Big number</p>
   }
+  if(count === 99) display.push(<p>Almost there</p>)
   return (
     <>
       
@@ -17,6 +19,7 @@ function App() {
       {count < 50 ? <DisplayNumber value={count} /> : <h1>Count is too high</h1>}   
       {count < 50 ? <DisplayNumber value={count} /> : null}
       {count >= 50 && <DisplayNumber value={count} />}
+      {display}
     </>
   )
 }
